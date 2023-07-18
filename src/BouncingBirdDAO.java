c74c95152f1f653a59f1221265988dd9f2c9b01e
ektotheo <138446648+ektotheo@users.noreply.github.com>
2023-07-19 00:05:52 +0200

    Update BouncingBirdDAO.java

--- src/BouncingBirdDAO.java
+++ src/BouncingBirdDAO.java
@@ -3,4 +3,10 @@ public interface BouncingBirdDAO {
     public void setterMethode(String typ, String pfad);
     public String getterMethode(String typ);
 
+
+    public void addRating( String username, String comments, int rating);
+    public String getRatings();
+
+
+    
 }
