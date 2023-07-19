d40573e6cebe951a53a97a46c1a2002937fb0292
ektotheo <138446648+ektotheo@users.noreply.github.com>
2023-07-19 09:14:39 +0200

    Update RateGameList.java

--- src/RateGameList.java
+++ src/RateGameList.java
@@ -20,7 +20,7 @@ public class RateGameList extends Observable {
     }
 
     public DefaultTableModel getRatingsTableModel() {
-        DefaultTableModel model = new DefaultTableModel(new String[]{"Username", "Comment", "Rating"}, 0);
+        DefaultTableModel model = new DefaultTableModel(new String[]{"Username", "Comments", "Rating"}, 0);
         List<Rating> ratings = renderReview();
 
         for (Rating rating : ratings) {
