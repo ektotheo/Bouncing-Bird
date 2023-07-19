491d345f505680ae3c07b656f693c3294f4fdfe0
ektotheo <138446648+ektotheo@users.noreply.github.com>
2023-07-19 21:01:34 +0200

    Update RateGameList.java

--- src/RateGameList.java
+++ src/RateGameList.java
@@ -1,12 +1,32 @@
+import javax.swing.table.DefaultTableModel;
+import java.util.List;
 import java.util.Observable;
 
 public class RateGameList extends Observable {
+    private BouncingBirdDBDAO dbDAO;
 
     public RateGameList() {
+        this.dbDAO = new BouncingBirdDBDAO();
     }
-   public void writeReviewFile(String bewertungstext, int sterne){
-   }
 
-    public void  renderReview(){
+    public void writeReviewFile(String username, String comments, int rating) {
+        dbDAO.addRating(username, comments, rating);
+        setChanged();
+        notifyObservers();
+    }
+
+    public List<Rating> renderReview() {
+        return dbDAO.getRatings();
+    }
+
+    public DefaultTableModel getRatingsTableModel() {
+        DefaultTableModel model = new DefaultTableModel(new String[]{"Username", "Comment", "Rating"}, 0);
+        List<Rating> ratings = renderReview();
+
+        for (Rating rating : ratings) {
+            model.addRow(new Object[]{rating.getUsername(), rating.getComment(), rating.getRating()});
+        }
+
+        return model;
     }
 }
