1b53dd0f032a42808929cbe855eabaefae9e1cde
ektotheo <138446648+ektotheo@users.noreply.github.com>
2023-07-19 21:50:21 +0200

    Update BouncingBirdDBDAO.java

--- src/BouncingBirdDBDAO.java
+++ src/BouncingBirdDBDAO.java
@@ -57,6 +57,49 @@ public class BouncingBirdDBDAO implements BouncingBirdDAO{
         }
     }
 
+public void addRating(String username, String comments, int rating) {
+    try (Connection connection = ora.getConnection()) {
+        String sql = "INSERT INTO RATINGS (USERNAME, COMMENTS, RATING, TIMESTAMP) VALUES (?, ?, ?, CURRENT_TIMESTAMP)";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1, username);
+        statement.setString(2, comments);
+        statement.setInt(3, rating);
+        int rowsUpdated = statement.executeUpdate();
+
+        if (rowsUpdated > 0) {
+            System.out.println("Erfolgreich aktualisiert");
+        } else {
+            System.out.println("Fehler beim Aktualisieren");
+        }
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+}
+
+public String getRatings() {
+    StringBuilder ratingsStringBuilder = new StringBuilder();
+    try (Connection connection = ora.getConnection();
+         Statement statement = connection.createStatement();
+         ResultSet resultSet = statement.executeQuery("SELECT * FROM RATINGS ORDER BY TIMESTAMP DESC")) {
+
+        while (resultSet.next()) {
+            String username = resultSet.getString("USERNAME");
+            String comment = resultSet.getString("COMMENTS");
+            int rating = resultSet.getInt("RATING");
+            Timestamp timestamp = resultSet.getTimestamp("TIMESTAMP");
+            ratingsStringBuilder.append("Username: ").append(username)
+                                .append(", Comment: ").append(comment)
+                                .append(", Rating: ").append(rating)
+                                .append(", Timestamp: ").append(timestamp)
+                                .append("\n");
+        }
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+    return ratingsStringBuilder.toString();
+
+
+    
     @Override
     public ArrayList<String> getScoreboard() {
 
